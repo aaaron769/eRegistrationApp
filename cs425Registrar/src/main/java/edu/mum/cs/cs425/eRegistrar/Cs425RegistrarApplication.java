7cfeb5af227e5b3153542ad31cdc559610685853
Aaron Gezai <aaaron769@gmail.com>
2019-06-05 01:42:38 -0500

    the final out put of the project

--- cs425Registrar/src/main/java/edu/mum/cs/cs425/eRegistrar/Cs425RegistrarApplication.java
+++ cs425Registrar/src/main/java/edu/mum/cs/cs425/eRegistrar/Cs425RegistrarApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Cs425RegistrarApplication {
-
+  // this is e registration Starter
 	public static void main(String[] args) {
 		SpringApplication.run(Cs425RegistrarApplication.class, args);
 	}
